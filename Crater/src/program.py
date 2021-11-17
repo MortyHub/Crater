74db65308ebfa6407b9b1c42a60c0ba6f17671c3
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-11-17 04:34:33 +0000

    Added Multi Line Feature

--- Crater/src/program.py
+++ Crater/src/program.py
@@ -1,9 +1,15 @@
+import random
+
 #################################
 # Command Detection
 #################################
 
-TOKENS = ['command: help', 'command:', 'command: import']
-IMPORTS = ['math', 'random', 'imp']
+TOKENS = ['command: help()', 'command:', 'command: import', 'command: multiline()']
+IMPORTS = ['random', 'imp']
+MULTITOK = []
+MULTITOKENS = ['import', 'log[', ']']
+y = False
+used = False
 
 def write(r):
 	if r == 'command:':
@@ -19,6 +25,45 @@ def write(r):
 		else:
 			importfun(IMPORTS[importt])
 			print('Importing ' + importt)
+	if r == TOKENS[3]:
+		line = 1
+		y = True
+		print('Type: end When You Finish The Script')
+		while(y == True):
+			mult = input(str(line) + ' >> ')
+			line += 1
+			if mult == 'end':
+				y = False
+				runc(MULTITOK)
+			else:
+				MULTITOK.append(mult)
+		
+def runc(c):
+
+	result = ' '
+	on = ' '
+	current = 0
+	for i in range(len(c)):
+		if c[current] == 'import':
+			on = 'import'
+		if on == 'import':
+			importfun(c[current])
+		
+		if c[current] == 'log[':
+			on = 'log'
+		if on == 'log':
+			on = 'text'
+		if on == 'text':
+			if c[current] == ']':
+				on = ' '
+				result = result.split("log[")
+				print(result[1])
+			elif on == 'text':
+				result += c[current]		
+		current += 1
+used = True
+
+
 
 #################################
 # Errors
@@ -32,15 +77,10 @@ def error(er):
 #################################
 
 def importfun(lib):
-	if lib == 'math':
-		math()
-	elif lib == 'random':
+	if lib == 'random':
 		rand()
 	elif lib == 'imp':
 		imp()
-
-def math():
-	print('math')
 
 def rand():
 	print('random')
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Crater.src.program import write
 
 version = '1.0.0'
 
-print('Crater ' + version + ' (default, Nov 16 2021, 20:56:28) For Help Please Type In command: help')
+print('Crater ' + version + ' (default, Nov 16 2021, 20:56:28) For Help Please Type In command: help()')
 while True:
 	instr = input('Crater >> ')
 	write(instr)
